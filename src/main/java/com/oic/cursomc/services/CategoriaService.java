cf1642df6a4ac993984c9346f3e6631d04a1a26d
RafaelMQs <rafaeelmq08@gmail.com>
2022-01-20 15:29:11 -0300

    Validação sintatica com o javax.validation

--- src/main/java/com/oic/cursomc/services/CategoriaService.java
+++ src/main/java/com/oic/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.oic.cursomc.domain.Categoria;
+import com.oic.cursomc.dto.CategoriaDTO;
 import com.oic.cursomc.repositories.CategoriaRepository;
 import com.oic.cursomc.services.exeptions.DataIntegrityExeption;
 import com.oic.cursomc.services.exeptions.ObjectNotFoundExeption;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
