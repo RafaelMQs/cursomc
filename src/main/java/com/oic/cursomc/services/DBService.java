68f07f1d716fafc03e408e4c2541de985938a028
RafaelMQs <rafaeelmq08@gmail.com>
2022-01-26 09:49:15 -0300

    Salvando perfil de usuarios na base de dados

--- src/main/java/com/oic/cursomc/services/DBService.java
+++ src/main/java/com/oic/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.oic.cursomc.domain.PagamentoComCartao;
 import com.oic.cursomc.domain.Pedido;
 import com.oic.cursomc.domain.Produto;
 import com.oic.cursomc.domain.enums.EstadoPagamento;
+import com.oic.cursomc.domain.enums.Perfil;
 import com.oic.cursomc.domain.enums.TipoCliente;
 import com.oic.cursomc.repositories.CategoriaRepository;
 import com.oic.cursomc.repositories.CidadeRepository;
@@ -113,17 +114,23 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "rafaeelmq08@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123") );
+		Cliente cli1 = new Cliente(null, "Maria Silva", "mariaSilva@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123") );
+		Cliente cli2 = new Cliente(null, "Rafael Martins", "rafaeelmq08@gmail.com", "95361120006", TipoCliente.PESSOAFISICA, pe.encode("123") );
+		cli2.addPerfil(Perfil.ADMIN);
 
+		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		cli2.getTelefones().addAll(Arrays.asList("24526688", "20785068"));
 
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2105", null, "Centro", "23777012", cli2, c2);
 
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
